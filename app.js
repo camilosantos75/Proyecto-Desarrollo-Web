2a463f38c39f8ce8d2fbecd0491ea0294c60fbb6
mauriciobiasizzo <mauricio.biasizzo@hotmail.com>
2023-04-23 21:32:42 -0300

    cambios Mauri

--- app.js
+++ app.js
@@ -13,12 +13,18 @@ $(document).ready(function() {
       if (meals) {
         for (var i = 0; i < meals.length; i++) {
           var meal = meals[i];
-          var html = '<div class="card mb-3"><img class="card-img-top" src="' + meal.strMealThumb + '"><div class="card-body"><h5 class="card-title">' + meal.strMeal + '</h5><p class="card-text">' + '</p></div></div>';
+          var html = '<div class="card mb-3" style="margin-bottom: 20px !important"><img class="card-img-top" src="' + 
+                      meal.strMealThumb +
+                      '"><div class="card-body"><h5 class="card-title">' +
+                      meal.strMeal +
+                      '</h5><p class="card-text">' + '</p></div></div>';
+
           if (i % 2 == 0) {
             htmlColumn1 += html;
           } else {
             htmlColumn2 += html;
           }
+
         }
       } else {
         htmlColumn1 = '<p>No se encontraron alimentos en la categoría seleccionada</p>';
